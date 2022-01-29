d7a8e236ef79a0da0a349720672edb1696a1a70b
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-29 14:41:47 -0300

    :wrench: feat: add fonts and routes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import GilroyBold from './assets/fonts/gilroy-bold.ttf';
+import GilroyHeavy from './assets/fonts/gilroy-heavy.ttf';
+import GilroyMedium from './assets/fonts/gilroy-medium.ttf';
+import GilroyRegular from './assets/fonts/gilroy-regular.ttf';
+import GilroySemiBold from './assets/fonts/gilroy-semibold.ttf';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [loaded] = useFonts({
+        GilroyBold,
+        GilroyHeavy,
+        GilroyMedium,
+        GilroyRegular,
+        GilroySemiBold,
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!loaded) return <AppLoading />;
+
+    return (
+        <>
+            <Routes />
+            <StatusBar style="auto" />
+        </>
+    );
+}
