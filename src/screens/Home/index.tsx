39d4854f1687445493f0d69ee3ad9c488537568b
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-31 17:56:26 -0300

    :wrench: feat: create screen Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,43 @@
+// import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Text, ScrollView } from 'react-native';
+
+import { ICharacters } from '../../@types';
+import data from '../../assets/application.json';
+import Categories from '../../components/Categories';
+import Header from '../../components/Header';
+import List from '../../components/List';
+import { styles } from './styles';
+
+export default function Home() {
+    const [heroes, setHeroes] = useState<ICharacters[]>([]);
+    const [villains, setVillains] = useState<ICharacters[]>([]);
+    const [antiHeroes, setAntiHeroes] = useState<ICharacters[]>([]);
+    const [aliens, setAliens] = useState<ICharacters[]>([]);
+    const [humans, setHumans] = useState<ICharacters[]>([]);
+
+    useEffect(() => {
+        setHeroes(data.heroes);
+        setVillains(data.villains);
+        setAntiHeroes(data.antiHeroes);
+        setAliens(data.aliens);
+        setHumans(data.humans);
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <ScrollView style={styles.container}>
+                <Text style={styles.welcome}>Bem vindo ao Marvel Heroes</Text>
+                <Text style={styles.title}>Escolha o seu {'\n'}personagem</Text>
+                <Categories />
+
+                <List category="Heróis" dataCategory={heroes} />
+                <List category="Vilões" dataCategory={villains} />
+                <List category="Anti-heróis" dataCategory={antiHeroes} />
+                <List category="Alienígenas" dataCategory={aliens} />
+                <List category="Humanos" dataCategory={humans} />
+            </ScrollView>
+        </>
+    );
+}
