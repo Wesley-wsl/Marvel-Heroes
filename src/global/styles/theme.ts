87aafa90754c62da43661f60ec57c6c847d93785
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-31 15:25:59 -0300

    :art: style: add theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        primaryRed: '#F2264B',
+        primaryDark: '#313140',
+        primaryGrey: '#B7B7C8',
+        primarySilver: '#F8F8F8',
+    },
+    fonts: {
+        GilroyBold: 'GilroyBold',
+        GilroyHeavy: 'GilroyHeavy',
+        GilroyMedium: 'GilroyMedium',
+        GilroyRegular: 'GilroyRegular',
+        GilroySemiBold: 'GilroySemiBold',
+    },
+};
