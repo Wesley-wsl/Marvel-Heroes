847795de5af8cdad8bc85099c1c3fd15a1fb8626
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-01 17:12:05 -0300

    :art: style: add style for Categories component

---
+++ src/components/Categories/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 32,
+        marginBottom: 48,
+        paddingRight: 24,
+    },
+    category: {
+        width: 56,
+        height: 56,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50,
+    },
+});
