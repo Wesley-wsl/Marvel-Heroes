d23d3e10473b2976b5d18ab9be0a160f5330a279
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-02 00:08:23 -0300

    :art: style: add style for HeaderBack component

---
+++ src/components/HeaderBack/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        paddingLeft: 24,
+        paddingTop: 40,
+        paddingBottom: 19,
+        zIndex: 999,
+    },
+    button: {
+        padding: 2,
+        maxWidth: 27,
+    },
+});
