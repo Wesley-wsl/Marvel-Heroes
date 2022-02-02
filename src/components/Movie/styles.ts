6a7413ef40539e8b57c2688053b9da85425825ed
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-02 00:10:18 -0300

    :art: style: add style for Movie component

---
+++ src/components/Movie/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        marginRight: 18,
+        borderRadius: 10,
+    },
+    image: {
+        width: 140,
+        height: 230,
+        borderRadius: 10,
+    },
+});
